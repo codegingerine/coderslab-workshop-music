d714c9c58fc5932c8075ba28c4410ed502c392d8
codegingerine <urskosik@gmail.com>
2016-08-24 13:27:17 +0200

    Boxes - hover updated/ Boxes - testing animated gif on hover 'sklep'

--- js/jq.js
+++ js/jq.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
 
-
-
-
-
-
+if (window.matchMedia("(min-width: 768px)").matches) {
+	$(function(){
+		$('.boxes .box-wrap img').hover(
+		  function() {
+		    $(this).attr('src', $(this).attr('data-anime'));
+		  }, function() {
+		    $(this).attr('src', $(this).attr('data-static'));
+		  }
+		);
+	});
+};
 
 });
 
